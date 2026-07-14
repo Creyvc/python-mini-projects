aae827175babd5964179b8d9fc2ca893c046fb04
Vuthy Cheam <creybluu@Creyy.local>
2026-07-14 18:28:25 +0700

    1

--- number-guessing-game.py/ngg.py
+++ number-guessing-game.py/ngg.py
@@ -1,2 +1,8 @@
-x = 1
-print(x)
+import random
+
+print("Welcome to the Number Guessing Game.")
+
+low = int(input("Enter the Lower Bound: "))
+high = int(input("Enter the Upper Bound: "))
+
+print(f"You have 10 chances to guess the number between {low} and {high}. Let's start!")
